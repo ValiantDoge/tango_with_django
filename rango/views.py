97b959a7b6842b5ba69a42edc3bd5d435b44b64d
Agnelo <agnelofernandes475@gmail.com>
2022-06-23 00:49:37 +0530

    added about commit2

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<center><h1 style=color:red;>Rango says hey partner</h1></center>")
+    return HttpResponse("<center><h1 style=color:red;>Rango says hey partner</h1><br><a href='/rango/about/'>About</a></center>")
+
+
+def about(request):
+    return HttpResponse("<center><h1>Rango says here is the about page.<br></h1><br><a href='/rango/'>Rango</a></h1></center>")
+
